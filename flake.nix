6aca8e7ce9531f258e1eb658fe65c84acd1bafdf
Mattis Hasler <mattis@pendor.de>
2023-12-19 08:41:16 +0100

    try to make composer happy

--- flake.nix
+++ flake.nix
@@ -10,23 +10,30 @@
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
   let
     pkgs = nixpkgs.legacyPackages.${system};
+    ssp = pkgs.php.buildComposerProject {
+        pname = "self-service-password";
+        version = "1.6.0";
+        src = self;
+        vendorHash = "";
+    };
     lum = pkgs.stdenvNoCC.mkDerivation {
       pname = "self-service-password";
       version = "1.5.4";
       src = self;
       installPhase = ''
         mkdir $out
-        cp -r www/* $out/
+        cp -r htdocs/ lang/ lib/ templates/ $out/
       '';
     };
   in {
     packages = {
-      ldap-user-manager = lum;
-      default = lum;
+      ssp = ssp;
+      default = ssp;
     };
-    devShells.php = pkgs.mkShell {
+    devShells.default = pkgs.mkShell {
       packages = with pkgs; [
         php
+        php83Packages.composer
       ];
     };
   });
